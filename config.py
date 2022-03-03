6123c8cfc8fc90cbc77a48fe8809667ada0ae342
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-03-03 09:49:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/Gameralpha/MYPERSONALEDIT.git"
+UPSTREAM_REPO = "https://github.com/Sunmusicking/Zaynkingmusic"
 UPSTREAM_BRANCH = "main"
 
 SUPPORT_CHANNEL = "https://t.me/Official_Devil_Support"
